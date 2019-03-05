6adcd1e9a995615f6b234e68b3b9e7ed21c400b2
Amber <415411212@qq.com>
2019-03-05 02:02:47 -0500

    Added homework. But there is an error which I don't understand...

---
+++ CodeLab1-Week5-HW/Assets/Scripts/FlowerSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerSpawn : MonoBehaviour
+{
+    public GameObject flowerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn",3,2);
+    }
+    
+    void Spawn()
+    {
+        GameObject newFlower = Instantiate(flowerPrefab);
+        newFlower.transform.position = new Vector2(Random.Range(-8,8),-1.65f);
+    }
+}
---
+++ CodeLab1-Week5-HW/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float MoveSpead;
+
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newForce = new Vector2(0, 0); //the force we will add to our player
+
+
+        if (Input.GetKey(leftKey)) //When someone presses "A"
+        {
+            newForce.x -= MoveSpead;
+        }
+
+        if (Input.GetKey(rightKey)) //When someone presses "D"
+        {
+            newForce.x += MoveSpead;
+        }
+
+        rb.AddForce(newForce);
+
+    }
+}
